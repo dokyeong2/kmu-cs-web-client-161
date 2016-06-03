be9bed6f9f416f28f287a0633712e76103fc927b
dokyeong2 <paruoruo@gmail.com>
2016-06-03 11:09:19 +0900

    .
    .

---
+++ answers.js
@@ -0,0 +1,16 @@
+function answer_1() {
+
+  document.getElementById("q1-answer").innerHTML = "<ul><li>D - Document</li><li>O - Object</li><li>M - Model</li></ul>";
+}
+
+
+function answer_2() {
+}
+
+
+function answer_3() {
+}
+
+
+function answer_4() {
+}
